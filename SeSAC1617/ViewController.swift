471fc65cbbe442d4457fa45753d7830e6b666843
Daniel <skansmfqh7312@gmail.com>
2022-10-18 09:41:26 +0900

    Initial Commit

---
+++ SeSAC1617/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC1617
+//
+//  Created by 권민서 on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
